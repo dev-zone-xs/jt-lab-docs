cdde286f8f1b685bae924154e1ea17a0c0f5e56d
Shutnik8 <8shiv888@gmail.com>
2025-10-03 11:08:09 +0500

    feat: Add custom domain support for docsru.jt-lab.com

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,10 +19,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://dev-zone-xs.github.io',
+  url: 'https://docsru.jt-lab.com',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/jt-lab-docs/',
+  // For custom domain, baseUrl should be '/'
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
